6b34e68be2055bd99040a76072c61bc464e1d47d
John Curley <curleyjw@dukes.jmu.edu>
2021-10-07 19:05:10 -0400

    Test

--- skibot_nav/setup.py
+++ skibot_nav/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'skibot_node = skibot.skibot_node:main'
+            'skibot_nav_node = skibot_nav.skibot_node:main'
         ],
     },
 )
