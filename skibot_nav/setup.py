1be7c9d12784f5a24adb96524798dd439ef09b86
John Curley <curleyjw@dukes.jmu.edu>
2021-10-07 15:46:19 -0400

    Setup addition

--- skibot_nav/setup.py
+++ skibot_nav/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-        'service = skibot_nav.service_member_function:skibot_nav_node',
+            'skibot_node = skibot.skibot_node:main'
         ],
     },
 )
